f2b7638b106d0b3ab68161033f02a7e1058c8468
imarco20 <marcode420@gmail.com>
2021-08-20 20:48:20 +0200

    implement show details handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"marcode.io/url-parser/pkg/forms"
 	"net/http"
 )
 
@@ -11,9 +11,39 @@ func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.renderTemplate(w, r, "home.page.tmpl", nil)
+	switch r.Method {
+	case http.MethodGet:
+		app.renderTemplate(w, r, "home.page.tmpl", &templateData{Form: forms.New(nil)})
+
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func (app *application) showDetailsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Here are the details about the submitted link")
+
+	switch r.Method {
+
+	case http.MethodPost:
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "invalid form input", http.StatusBadRequest)
+			return
+		}
+
+		form := forms.New(r.PostForm)
+		form.Required("link")
+		form.MatchesPattern("link", forms.URLRegExp)
+
+		if !form.Valid() {
+			app.renderTemplate(w, r, "home.page.tmpl", &templateData{Form: form})
+		} else {
+			details := getLinkDetails(form.Values.Get("link"))
+			app.renderTemplate(w, r, "details.page.tmpl", &templateData{Link: details})
+		}
+
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 }
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,8 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"marcode.io/url-parser/pkg/models"
+	"marcode.io/url-parser/pkg/parser"
+	"net/http"
+)
 
-func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, templateName string, templateData interface{}) {
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, templateName string, templateData *templateData) {
 	templateSet, ok := app.templatesCache[templateName]
 	if !ok {
 		http.Error(w, "error rendering template files", http.StatusInternalServerError)
@@ -14,4 +22,37 @@ func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, t
 		http.Error(w, "error rendering template files", http.StatusInternalServerError)
 		return
 	}
+}
+
+func getLinkDetails(url string) models.LinkDetails {
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Printf("error fetching url: %v", err)
+	}
+
+	responseBody, _ := io.ReadAll(response.Body)
+	fmt.Println(string(responseBody))
+
+	var details models.LinkDetails
+
+	details.PageURL = url
+
+	version, _ := parser.FindHTMLVersion(bytes.NewReader(responseBody))
+	details.HTMLVersion = version
+
+	title, _ := parser.FindTitle(bytes.NewReader(responseBody))
+	details.Title = title
+
+	headings, err := parser.FindAllHeadings(bytes.NewReader(responseBody))
+	details.Headings = headings
+
+	links, _ := parser.FindAllLinks(bytes.NewReader(responseBody), url)
+	details.Links = links
+
+	hasLoginForm, _ := parser.CheckIfPageHasLoginForm(bytes.NewReader(responseBody))
+	details.HasLoginForm = hasLoginForm
+
+	return details
 }

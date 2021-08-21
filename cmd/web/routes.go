a65ed7ab94607bbb66487cf4a67fdf2e5c68efb8
imarco20 <marcode420@gmail.com>
2021-08-21 16:04:21 +0200

    add stylesheet file and update template styles

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,5 +8,8 @@ func (app *application) routes() http.Handler {
 	router.Handle("/", http.HandlerFunc(app.homeHandler))
 	router.Handle("/details", http.HandlerFunc(app.showDetailsHandler))
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	return router
 }
